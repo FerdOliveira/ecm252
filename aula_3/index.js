1a75ca194789efeffbee0cfb167f66e116ca494d
Felipe Villani <felipevillani.2000@hotmail.com>
2022-03-05 13:00:26 -0300

    Update

--- aula_3/index.js
+++ aula_3/index.js
@@ -30,3 +30,87 @@ function map(v, f){
     return retorno
 }
 console.log(map(['abcd', 'abc'], e => e.length))
+
+
+function eAgora(){
+    let cont = 1
+    function f1(){
+        console.log(cont)
+    }
+    cont++
+    function f2(){
+        console.log(cont)
+    }
+    // Isso é um objeto JSON
+    return {f1,f2}
+}
+
+let res = eAgora()
+res.f1()
+res.f2()
+
+// Declaração e funções
+
+ const minhaFuncao = function (a,b){
+     return a+b
+ }
+ const minhaFuncaoDois = (a,b) => a + b
+ const calculo = {
+     soma: (a, b) => a + b,
+     sub: (a, b) => a - b
+ }
+console.log(calculo.soma(1,2))
+
+// JSON
+
+let c = {
+    cnpj: 51186566833,
+    endereco: {
+        logradouro: 'Rua B',
+        numero: 122,
+        bairro: 'Vila A'
+    },
+    veiculos: [
+        {
+            marca: 'Ford',
+            modelo: 'Ka',
+            ano: 1995,
+            revisoes: [
+                {
+                    data: '14/02/1996',
+                    consultor: 'Fábio'
+                },
+                {
+                    data: '13/02/1986',
+                    consultor: 'João'
+                }
+            ]
+        }      
+    ]
+}
+console.log(c.veiculos[0].revisoes[1].consultor)
+
+
+// Funções callback
+
+const fs = require('fs')
+const abrirArquivo = function (nomeArquivo){
+    const exibirConteudo = (erro, conteudo) => {
+        if(erro)
+            console.log(`Deu erro: ${erro}`)
+        else{
+            console.log(`Conteúdo: ${conteudo.toString()}`)
+            const triplo = +conteudo.toString() * 3
+            const finalizar = (erro) => {
+                if(erro)
+                    console.log("Erro ao salvar o triplo")
+                else
+                    console.log('Salvou com sucesso')
+            }
+            fs.writeFile('triplo.txt', triplo.toString(), finalizar)
+        }
+    }
+    fs.readFile(nomeArquivo, exibirConteudo)
+    
+}
+abrirArquivo("arquivo.txt")
