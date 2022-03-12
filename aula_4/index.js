9acc5530348d83a06717decee927238274dea3cf
Felipe Villani <felipevillani.2000@hotmail.com>
2022-03-12 13:09:04 -0300

    Update

--- aula_4/index.js
+++ aula_4/index.js
@@ -1,8 +1,27 @@
 require('dotenv').config()
+const axios = require('axios')
 
 const { APIKEY, PROTOCOL, BASE_URL, LANGUAGE, UNITS } = process.env
 
 
 city = 'Itupeva'
-const urlApi = `${PROTOCOL}://${BASE_URL}?q=${city}&appid=${process.env.APIKEYWEATHER}&lang=${LANGUAGE}&units=${UNITS}`
-console.log(urlApi)
+const url = `${PROTOCOL}://${BASE_URL}?q=${city}&appid=${process.env.APIKEYWEATHER}&lang=${LANGUAGE}&units=${UNITS}`
+
+
+// IO-Bound
+axios.get(url)
+.then(res => {
+    return res.data
+})
+.then(res => {
+    return res.cnt
+})
+.then(res => {
+    return res.list
+})
+ .then(res => {
+     for(let previsao in res){
+        if(previsao.main.feels_like >= 0)
+            console.log("EITA QUENTINHO!")
+     }
+ })
